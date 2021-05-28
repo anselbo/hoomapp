1229eb8dbca63254914ec1e9353d9516145a9724
ansel <anselj37@gmail.com>
2021-05-28 04:22:16 +0100

    static files added

--- homapp_project/settings.py
+++ homapp_project/settings.py
@@ -165,8 +165,13 @@ DECIMAL_SEPARATOR = ','
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 STATIC_URL = '/static/'
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
 
 """ For media url and for media root """
 MEDIA_URL = '/media/'
