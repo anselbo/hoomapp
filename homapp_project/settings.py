b3977c54070708b516ca057a9eb1ea9b375416d5
ansel <anselj37@gmail.com>
2021-05-28 16:30:39 +0100

    changes againgg

--- homapp_project/settings.py
+++ homapp_project/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 import os 
 from decouple import config
 
-import dj_database_url
+
 
 
 from pathlib import Path
@@ -118,7 +118,7 @@ DATABASES = {
 
     }
 }
-
+import dj_database_url
 db_from_env = dj_database_url.config(conn_max_age=600)
 DATABASES['default'].update(db_from_env)
 
