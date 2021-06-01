be62fd477e00123b01fd5220269f71400d28d141
anselbo <anselj37@gmail.com>
2021-06-01 12:51:34 +0100

    jquery-ui removed entirely

---
+++ staticfiles/account/my_js/own.2db55f01e90f.js
@@ -0,0 +1,12 @@
+// var cleave = new Cleave(".goo", {
+//   numeral: true,
+//   numeralThousandsGroupStyle: "thousand",
+// });
+
+$(document).ready(function () {
+  var date_input = $('input[name="due"]');
+  var options = {
+    dateFormat: "yy-mm-dd",
+  };
+  date_input.datepicker(options);
+});
