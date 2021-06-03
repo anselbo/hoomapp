143b545d79f26b054e8647e5551be45f2357d20e
anselbo <anselj37@gmail.com>
2021-06-03 03:35:16 +0100

    environment variables for email credentials added

--- homapp_project/settings.py
+++ homapp_project/settings.py
@@ -206,8 +206,9 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'homapp27@gmail.com'
-EMAIL_HOST_PASSWORD = 'pmppneqnchinfaff'
+EMAIL_HOST_USER = os.environ.get("HOMAPP_EMAIL_USERNAME")
+EMAIL_HOST_PASSWORD = os.environ.get("HOMAPP_EMAIL_PASSWORD")
+
 
 
 
